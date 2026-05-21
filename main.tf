830a206b973525036fa5b1a09d41465a421d8eb3
Nagaraju Kasturi <31821089+Naga592@users.noreply.github.com>
2026-05-21 09:18:50 -0600

    Add Azure provider configuration to main.tf
    Specify the Azure provider source and version in Terraform configuration.

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
     resource_group {
