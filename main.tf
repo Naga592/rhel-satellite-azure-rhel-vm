26d40fd3fb1a3739c250b15f68bd54e61f312e6e
Nagaraju Kasturi <31821089+Naga592@users.noreply.github.com>
2026-05-21 11:37:46 -0600

    Add use_azure_cli variable to main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
     storage_account_name = "satfstatenew"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_azure_cli        = false
   }
 }
 
