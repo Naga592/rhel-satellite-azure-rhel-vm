e33eb3951ad504fcc188389b759957d00b191cef
Nagaraju Kasturi <31821089+Naga592@users.noreply.github.com>
2026-05-25 14:45:23 -0600

    Remove Azure provider and backend configuration
    Removed Azure provider configuration and backend settings.

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
-  }
-}
-
-#  backend "azurerm" {
-#   resource_group_name  = "tfstate-rg"
-#    storage_account_name = "satfstatenew"
-#    container_name       = "tfstate"
-#    key                  = "terraform.tfstate"
-#    use_azuread_auth     = true
-#  }
-#}
-
 # Input variable for Azure subscription ID
 variable "subscription_id" {
   type = string
